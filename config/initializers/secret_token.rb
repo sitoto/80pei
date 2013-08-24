588f15e6ddc53a48d711ea7c8adf8135cd8d4637
sitoto <huang.gadela@gmail.com>
2013-08-24 23:34:45 +0800

    use deface

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Balingpei::Application.config.secret_token = '713db4ab9dd689751bc2be4f4a8cd286eeec12e82ccc73a8def909d316d04bb11c6b99eac3043746683087f0820fa9c9fb3ce7f0a83423410b4e070332f0d973'
