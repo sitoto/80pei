19d07f7d7c0eadab78fedd6ea72e27ea08b343d2
sitoto <huang.gadela@gmail.com>
2013-09-03 21:48:39 +0800

    deploy update

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,17 +15,39 @@ role :web, "42.121.0.135"
 role :app, "42.121.0.135"
 role :db,  "42.121.0.135", :primary => true 
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+set :unicorn_path, "#{deploy_to}/current/config/unicorn.rb"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  task :start, :roles => :app do
+    run "cd #{deploy_to}/current/; RAILS_ENV=production bundle exec unicorn_rails -c #{unicorn_path} -D"
+  end
+
+  task :stop, :roles => :app do
+    run "kill -QUIT `cat #{deploy_to}/current/tmp/pids/unicorn.pid`"
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "kill -USR2 `cat #{deploy_to}/current/tmp/pids/unicorn.pid`"
   end
 end
+
+
+task :init_shared_path, :roles => :web do
+  run "mkdir -p #{deploy_to}/shared/log"
+  run "mkdir -p #{deploy_to}/shared/pids"
+  run "mkdir -p #{deploy_to}/shared/assets"
+end
+
+task :link_shared_files, :roles => :web do
+  run "ln -sf #{deploy_to}/shared/config/*.yml #{deploy_to}/current/config/"
+  run "ln -sf #{deploy_to}/shared/config/unicorn.rb #{deploy_to}/current/config/"
+  run "ln -sf #{deploy_to}/shared/config/initializers/secret_token.rb #{deploy_to}/current/config/initializers"
+  run "ln -sf #{deploy_to}/shared/config/faye_thin.yml #{deploy_to}/current/faye_server/thin.yml"
+end
+
+task :compile_assets, :roles => :web do
+  run "cd #{deploy_to}/current/; RAILS_ENV=production bundle exec rake assets:precompile"
+end
+
+after "deploy:finalize_update","deploy:symlink", :init_shared_path, :link_shared_files, :compile_assets
